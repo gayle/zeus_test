38e4c3fbc1b2b78378376a0f362276dcd2254768
Gayle Bender <gayle.l.bender@jpmorgan.com>
2015-02-26 14:22:30 -0500

    generated rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZeusTest::Application.config.secret_key_base = '264babe52f1546639ec16e55cd85c5d958aa99ee35265f5efeace8aeb3d359381530f79395b73ae9404a7b664e3e1679e4931d1957f453054b9d273d67a06da3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZeusTest::Application.config.session_store :cookie_store, key: '_zeus_test_session'
